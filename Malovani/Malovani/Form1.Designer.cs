45158a19e1ed0d8e839565d1f102c58b312cc041
Fikusak <144894550+Fikusak@users.noreply.github.com>
2024-03-23 17:28:42 +0100

    Add files via upload

--- Malovani/Malovani/Form1.Designer.cs
+++ Malovani/Malovani/Form1.Designer.cs
@@ -32,9 +32,17 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -50,9 +58,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(59, 62);
+            this.button2.Location = new System.Drawing.Point(28, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 26);
+            this.button2.Size = new System.Drawing.Size(92, 26);
             this.button2.TabIndex = 1;
             this.button2.Text = "Change colour";
             this.button2.UseVisualStyleBackColor = true;
@@ -61,9 +69,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(28, 65);
+            this.button3.Location = new System.Drawing.Point(100, 33);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(25, 23);
+            this.button3.Size = new System.Drawing.Size(50, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Eraser";
             this.button3.UseVisualStyleBackColor = true;
@@ -71,24 +79,14 @@
             // 
             // panel1
             // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Location = new System.Drawing.Point(5, 94);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(789, 352);
+            this.panel1.Size = new System.Drawing.Size(790, 350);
             this.panel1.TabIndex = 3;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
             this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(115, 33);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(53, 25);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Create a shape";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button4_MouseMove);
             // 
             // comboBox1
             // 
@@ -104,27 +102,124 @@
             "8",
             "9",
             "10"});
-            this.comboBox1.Location = new System.Drawing.Point(63, 37);
+            this.comboBox1.Location = new System.Drawing.Point(59, 33);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(30, 21);
+            this.comboBox1.Size = new System.Drawing.Size(35, 21);
             this.comboBox1.TabIndex = 5;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(706, 31);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 27);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "New picture";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(327, 33);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(48, 24);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Ellipse";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(328, 66);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(64, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Rectangle";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(381, 33);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(36, 24);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Line";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(445, 38);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.Text = "unfilled";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(445, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(46, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "filled";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(570, 33);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(91, 24);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Upload Image";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Shapes";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,9 +229,17 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Malovani/Malovani/Form1.cs
+++ Malovani/Malovani/Form1.cs
@@ -14,41 +14,43 @@ namespace Malovani
     public partial class Form1 : Form
     {
         Pen p;
+        SolidBrush b;
         Graphics g;
         bool paint = false;
+        float size;
         Point px, py;
-        int index;
+        int index = 1;
         Pen er;
+        int x, y, fX, fY, lX, lY;
+        int fill;
+        
         public Form1()
         {
             InitializeComponent();
             this.BackColor = Color.Gray;
             panel1.BackColor = Color.White;
             g = panel1.CreateGraphics();
+            comboBox1.Text = "1";
+            textBox1.BackColor = Color.Black;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            float size = float.Parse(comboBox1.Text);
-            p = new Pen(Color.Black, size);
-            er = new Pen(Color.White, size);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-         
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-          
-            
+            size = float.Parse(comboBox1.Text);
+            p = new Pen(colorDialog1.Color, size);
+            er = new Pen(Color.White, 15);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             index = 0;
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            colorDialog1.ShowDialog();
+            textBox1.BackColor = colorDialog1.Color;
+            p = new Pen(colorDialog1.Color, size);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -62,22 +64,58 @@ namespace Malovani
             ToolTip1.SetToolTip(this.button1, this.button1.Text);     
         }
 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+
         private void button2_MouseHover(object sender, EventArgs e)
         {
             System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
             ToolTip1.SetToolTip(this.button2, this.button2.Text);
         }
 
-        private void button4_MouseMove(object sender, MouseEventArgs e)
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
-            ToolTip1.SetToolTip(this.button4, this.button4.Text);
+            fill = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            fill = 1;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string imageLocation = "";
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "jpg files (*.jpg)|*.jpg| PNG files (*.png)|*.png";
+                
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                imageLocation = dialog.FileName;
+                panel1.BackgroundImage = Image.FromFile(imageLocation);
+                panel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            }
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
             paint = true;
             py = e.Location;
+
+            fX = e.X;
+            fY = e.Y;
         }
 
         private void panel1_MouseMove(object sender, MouseEventArgs e)
@@ -90,27 +128,54 @@ namespace Malovani
                     g.DrawLine(p, px, py);
                     py = px;
                 }
-                else if(index==0)
+                if(index==0)
                 {
                     px = e.Location;
                     g.DrawLine(er, px, py);
                     py = px;
                 }
             }
-            
+
+            x = e.X;
+            y = e.Y;
+            lX = e.X - fX;
+            lY = e.Y - fY;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
-            p.Color = colorDialog1.Color;
+            g.Clear(Color.White);
         }
 
         private void panel1_MouseUp(object sender, MouseEventArgs e)
         {
             paint = false;
-        }
 
-        
+            b = new SolidBrush(colorDialog1.Color);
+            lX = x - fX;
+            lY = y - fY;
+
+            if (index == 2 && fill == 0)
+            {
+                g.DrawEllipse(p, fX, fY, lX, lY);
+            }
+            else if (index == 2 && fill == 1)
+            {
+                g.FillEllipse(b, fX, fY, lX, lY);
+            }
+            if (index == 3 && fill == 0)
+            {
+                g.DrawRectangle(p, fX, fY, lX, lY);
+            }
+            else if (index == 3 && fill == 1)
+            {
+                g.FillRectangle(b, fX, fY, lX, lY);
+            }
+            if (index == 4)
+            {
+                g.DrawLine(p, fX, fY, x, y);
+            }
+        }
     }
     
 }
